611b4d9e0a9669d5c1cc189a7eb9c575917e4902
Javierhb16 <javier.hidalgo1605@gmail.com>
2026-07-26 19:38:16 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let computerChoice = Math.random();
     if (computerChoice < 0.33) {
@@ -12,3 +15,35 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Rock, Paper or Scissors?").toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    // Check ties first
+    if (humanChoice == "rock" && computerChoice == "rock") {
+        console.log("It's a tie!");
+    } else if (humanChoice == "paper" && computerChoice == "paper") {
+        console.log("It's a tie!");
+    } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        console.log("It's a tie!");
+    }
+    // Check player wins
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock");
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Scissors");
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats Paper");
+    }
+    // Check computer wins
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock");
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats Scissors");
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats Paper");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
