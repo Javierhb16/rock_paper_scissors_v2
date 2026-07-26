8bd5f0a3420595fbe572acd712b43dfd77ff7ee0
Javierhb16 <javier.hidalgo1605@gmail.com>
2026-07-26 19:27:42 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let computerChoice = Math.random();
+    if (computerChoice < 0.33) {
+        return "rock";
+    }
+    if (computerChoice < 0.66) {
+        return "paper";
+    }
+    return "scissors";
+}
