f548ad6270211ffca158cd1d345476b6fb00a421
Javierhb16 <javier.hidalgo1605@gmail.com>
2026-07-26 19:55:59 +0200

    Add playGame function

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let computerChoice = Math.random();
     if (computerChoice < 0.33) {
@@ -16,34 +13,56 @@ function getHumanChoice() {
     return prompt("Rock, Paper or Scissors?").toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    // Check ties first
-    if (humanChoice == "rock" && computerChoice == "rock") {
-        console.log("It's a tie!");
-    } else if (humanChoice == "paper" && computerChoice == "paper") {
-        console.log("It's a tie!");
-    } else if (humanChoice == "scissors" && computerChoice == "scissors") {
-        console.log("It's a tie!");
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice) {
+        // Check ties first
+        if (humanChoice == "rock" && computerChoice == "rock") {
+            console.log("It's a tie!");
+        } else if (humanChoice == "paper" && computerChoice == "paper") {
+            console.log("It's a tie!");
+        } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+            console.log("It's a tie!");
+        }
+        // Check player wins
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+        } else if (humanChoice == "rock" && computerChoice == "scissors") {
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+        } else if (humanChoice == "scissors" && computerChoice == "paper") {
+            console.log("You win! Scissors beats Paper");
+            humanScore++;
+        }
+        // Check computer wins
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        } else if (humanChoice == "scissors" && computerChoice == "rock") {
+            console.log("You lose! Rock beats Scissors");
+            computerScore++;
+        } else if (humanChoice == "paper" && computerChoice == "scissors") {
+            console.log("You lose! Scissors beats Paper");
+            computerScore++;
+        } 
     }
-    // Check player wins
-    else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! Paper beats Rock");
-    } else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win! Rock beats Scissors");
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! Scissors beats Paper");
+
+    function checkWinner(humanFinalScore, computerFinalScore) {
+        if (humanFinalScore > computerFinalScore) return "You won! The final score is: " + humanFinalScore + "-" + computerFinalScore;
+        else if (humanFinalScore < computerFinalScore) return "You lost! The final score is: " + humanFinalScore + "-" + computerFinalScore;
+        else return "Tie! The final score is: " + humanFinalScore + "-" + computerFinalScore;
     }
-    // Check computer wins
-    else if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! Paper beats Rock");
-    } else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose! Rock beats Scissors");
-    } else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! Scissors beats Paper");
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
+
+    console.log(checkWinner(humanScore, computerScore));
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
